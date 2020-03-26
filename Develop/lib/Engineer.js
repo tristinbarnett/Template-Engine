9a5aef2597a5de67b9696d09cbed78ba50b6a775
Tristin Barnett <tristinbarnett@gmail.com>
2020-03-26 12:55:43 -0700

    completed empoyee file

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,9 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+Employee = require("../lib/Employee.js");
+
+class Engineer extends Employee{
+    constructor(github){
+        super(name, id, email);
+        this.github = github;
+    }
+}
